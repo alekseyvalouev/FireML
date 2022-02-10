0c77ed1f6dc0217cd44bfb03ffbf1f834495e752
Aleksey Valouev <alekseyvalouev@gmail.com>
2022-02-09 22:51:13 -0800

    fixed auto sampling script

--- Scripts/auto_raster_sampling.py
+++ Scripts/auto_raster_sampling.py
@@ -1,6 +1,8 @@
-import glob, os, qgis.analysis
+import glob, os, qgis.analysis, qgis.analysis
 vectorlayer = qgis.utils.iface.mapCanvas().currentLayer()
 rasterfolder = '/Users/alekseyvalouev/Desktop/FireData/GIS/Percipitation_Data/'
 os.chdir(rasterfolder)
-for lyr in glob.glob("*.bil"):
-    qgis.analysis.QgsZonalStatistics(vectorlayer, lyr, attributePrefix=lyr, rasterBand=1, stats=QgsZonalStatistics.Statistics(QgsZonalStatistics.Mean)).calculateStatistics(None)
+for lyr in sorted(glob.glob('*.bil')):
+    raster_layer = QgsRasterLayer(lyr)
+    pref = lyr[-12:-8] + "_percip_"
+    qgis.analysis.QgsZonalStatistics(vectorlayer, raster_layer, attributePrefix=pref, rasterBand=1, stats=QgsZonalStatistics.Statistics(QgsZonalStatistics.Mean)).calculateStatistics(None)
