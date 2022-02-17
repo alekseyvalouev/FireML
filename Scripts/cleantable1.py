92caf91b168f9e122bf132104e024914bf2881fc
Aleksey Valouev <alekseyvalouev@gmail.com>
2022-02-17 11:09:32 -0800

    removed any columns with null date values

--- Scripts/cleantable1.py
+++ Scripts/cleantable1.py
@@ -4,4 +4,6 @@ data= pd.read_csv('/Users/alekseyvalouev/Desktop/FireData/CSV/table1.csv')
 
 out_data = data.drop_duplicates(subset=['OBJECTID'], keep='first')
 
+out_data.dropna(subset=["ALARM_DATE", "CONT_DATE"], how='all')
+
 out_data.to_csv('/Users/alekseyvalouev/Desktop/FireData/CSV/finaltable1.csv', index=False)
