aa20b7dd4c32001eb675bc5bc69122395aea3dd7
Aleksey Valouev <alekseyvalouev@gmail.com>
2022-02-18 11:24:42 -0800

    kept duplicates in order to better represent fires

--- Scripts/.ipynb_checkpoints/cleantable1-checkpoint.py
+++ Scripts/.ipynb_checkpoints/cleantable1-checkpoint.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 data= pd.read_csv('/Users/alekseyvalouev/Desktop/FireData/CSV/table1.csv')
 
-out_data = data.drop_duplicates(subset=['OBJECTID'], keep='first')
+# out_data = data.drop_duplicates(subset=['OBJECTID'], keep='first')
+
+out_data = data
 
 out_data.dropna(axis = 0, subset=["ALARM_DATE", "CONT_DATE"], how='all')
 
