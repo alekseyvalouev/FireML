b76afa076be8b60575fea6e189424c7545fa8518
Aleksey Valouev <alekseyvalouev@gmail.com>
2022-03-27 01:53:29 -0700

    recommited in order to get around filesize limit

--- Scripts/.ipynb_checkpoints/table2_cleaning-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data= pd.read_csv("/Users/alekseyvalouev/Desktop/FireData/CSV/table2.csv")
-
-data = data.dropna(axis=0, subset=['popdensity'])
-
-out_data = pd.DataFrame(data)
-
-out_data.to_csv('/Users/alekseyvalouev/Desktop/FireData/CSV/popdensitycleanedtable2.csv', index=False)
